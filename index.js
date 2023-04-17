0d5ebfc13e6a1175da22836dbe34cedbe31b9cce
KoffeeKat <124860643+K0ffeeKat@users.noreply.github.com>
2023-04-17 18:09:24 +0500

    Deploy the web-app - v1.0
    Things added:
        - Firebase
        - List of items below 4 icons

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL : "https://realtime-database-45940-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+let shoppingListEl = document.querySelector("#shopping-list")
 const inputField = document.querySelector("#input-field")
 const addBtn = document.querySelector("#add-button")
 
 addBtn.addEventListener("click", function(){
     let inputValue = inputField.value
-    console.log(inputValue)
+    push(shoppingListInDB, inputValue)
+    clearInputField()
+    
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        shoppingListEl.setAttribute("style", "margin: 0")
+        let itemsArray = Object.entries(snapshot.val())
+        clearShoppingList()
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            addToList(currentItem)
+    }
+    }else{
+        shoppingListEl.setAttribute("style", "margin: 20px auto; color:#5b8291;user-select:none")
+        shoppingListEl.innerHTML = "No items here... yet."
+
+    }
+})
+
+function clearShoppingList() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputField() {
+    inputField.value = ""
+}
+
+function addToList(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    newEl.addEventListener("click", function(){
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
+    shoppingListEl.append(newEl)
+}
